e1afece0eee5ad87532bfca046c5d903b692a181
J <jprihie1208@g.educaand.es>
2024-03-04 16:30:49 +0100

    Subo la primera versión de Calculadora

---
+++ src/calculadora4/pkg2/Calculadora.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora4.pkg2;
+
+/**
+ *
+ * @author Alumno Tarde
+ */
+public class Calculadora {
+    
+    public int sumar(int num1, int num2){
+        return num1+num2;
+    
+    }
+    
+    public int restar(int num1, int num2){
+        return num1+num2;
+    
+    }
+    
+    public int multiplicar(int num1, int num2){
+        return num1*num2;
+    
+    }
+}
