b717d1f675c00182134ca08d16bdb21a67f2b895
Dani <dani@example.com>
2024-03-06 17:53:43 +0100

    Calculadora con suma, resta y multiplicación

--- src/calculadora4/pkg2/Calculadora.java
+++ src/calculadora4/pkg2/Calculadora.java
@@ -10,12 +10,16 @@ package calculadora4.pkg2;
  */
 public class Calculadora {
 
-    public int sumar(int num1, int num2){
-        return num1+num2;
+    public int sumar(int num1, int num2) {
+        return num1 + num2;
 
     }
-    
-    public double multiplicar(int num1, int num2){
-        return num1*num2;
+
+    public int restar(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public double multiplicar(int num1, int num2) {
+        return num1 * num2;
     }
 }
